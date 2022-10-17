25e4506aac4277f4d68d32ae9ef8636947b44894
Faran Babar <68579788+faranbabar@users.noreply.github.com>
2022-10-17 15:38:58 -0500

    Initial commit

---
+++ Mail Merge Project Start/main.py
@@ -0,0 +1,13 @@
+PLACE_HOLDER = "[name]"
+
+with open("./Input/Names/invited_names.txt") as names_file:
+    names = names_file.readlines()
+
+with open("./Input/Letters/starting_letter.txt") as letter_file:
+    letter = letter_file.read()
+    for name in names:
+        striped_name = name.strip()
+        new_letter = letter.replace(PLACE_HOLDER, striped_name)
+        with open(f"./Output/ReadyToSend/letter_for_{striped_name}.txt", mode="w") as completed_letter:
+            completed_letter.write(new_letter)
+
